480f61dfe331107331995cf132f40a59d8de4448
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2019-01-09 20:55:03 +0200

    Add files via upload

---
+++ Tests/Test One/Part3/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Part3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RnNum1 = new System.Windows.Forms.Label();
+            this.RnNum2 = new System.Windows.Forms.Label();
+            this.RnNum3 = new System.Windows.Forms.Label();
+            this.RnNum4 = new System.Windows.Forms.Label();
+            this.RnNum5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.FinishBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ScoreList = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(436, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "&Game";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Enabled = false;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "&New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // RnNum1
+            // 
+            this.RnNum1.AutoSize = true;
+            this.RnNum1.Location = new System.Drawing.Point(68, 51);
+            this.RnNum1.Name = "RnNum1";
+            this.RnNum1.Size = new System.Drawing.Size(28, 13);
+            this.RnNum1.TabIndex = 1;
+            this.RnNum1.Text = "###";
+            // 
+            // RnNum2
+            // 
+            this.RnNum2.AutoSize = true;
+            this.RnNum2.Location = new System.Drawing.Point(135, 51);
+            this.RnNum2.Name = "RnNum2";
+            this.RnNum2.Size = new System.Drawing.Size(28, 13);
+            this.RnNum2.TabIndex = 2;
+            this.RnNum2.Text = "###";
+            // 
+            // RnNum3
+            // 
+            this.RnNum3.AutoSize = true;
+            this.RnNum3.Location = new System.Drawing.Point(202, 51);
+            this.RnNum3.Name = "RnNum3";
+            this.RnNum3.Size = new System.Drawing.Size(28, 13);
+            this.RnNum3.TabIndex = 3;
+            this.RnNum3.Text = "###";
+            // 
+            // RnNum4
+            // 
+            this.RnNum4.AutoSize = true;
+            this.RnNum4.Location = new System.Drawing.Point(269, 51);
+            this.RnNum4.Name = "RnNum4";
+            this.RnNum4.Size = new System.Drawing.Size(28, 13);
+            this.RnNum4.TabIndex = 4;
+            this.RnNum4.Text = "###";
+            // 
+            // RnNum5
+            // 
+            this.RnNum5.AutoSize = true;
+            this.RnNum5.Location = new System.Drawing.Point(336, 51);
+            this.RnNum5.Name = "RnNum5";
+            this.RnNum5.Size = new System.Drawing.Size(28, 13);
+            this.RnNum5.TabIndex = 5;
+            this.RnNum5.Text = "###";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(25, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(25, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(202, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(25, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(269, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(25, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(336, 79);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(25, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(219, 122);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 11;
+            this.StartBtn.Text = "&Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // FinishBtn
+            // 
+            this.FinishBtn.Location = new System.Drawing.Point(313, 122);
+            this.FinishBtn.Name = "FinishBtn";
+            this.FinishBtn.Size = new System.Drawing.Size(75, 23);
+            this.FinishBtn.TabIndex = 12;
+            this.FinishBtn.Text = "&Finish";
+            this.FinishBtn.UseVisualStyleBackColor = true;
+            this.FinishBtn.Click += new System.EventHandler(this.FinishBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(65, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Score List:";
+            // 
+            // ScoreList
+            // 
+            this.ScoreList.FormattingEnabled = true;
+            this.ScoreList.Location = new System.Drawing.Point(66, 204);
+            this.ScoreList.Name = "ScoreList";
+            this.ScoreList.Size = new System.Drawing.Size(322, 134);
+            this.ScoreList.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 359);
+            this.Controls.Add(this.ScoreList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FinishBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RnNum5);
+            this.Controls.Add(this.RnNum4);
+            this.Controls.Add(this.RnNum3);
+            this.Controls.Add(this.RnNum2);
+            this.Controls.Add(this.RnNum1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Memory Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label RnNum1;
+        private System.Windows.Forms.Label RnNum2;
+        private System.Windows.Forms.Label RnNum3;
+        private System.Windows.Forms.Label RnNum4;
+        private System.Windows.Forms.Label RnNum5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button FinishBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ScoreList;
+    }
+}
+
---
+++ Tests/Test One/Part3/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part3
+{
+    public partial class Form1 : Form
+    {
+        static int LabelAmount = 5;
+        Random randomGen = new Random();
+        int[] labelValues = new int[LabelAmount];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValueGenerator();
+            LabelNumGenerator();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (RnNum1.Visible)
+            {
+                FlipVisible();
+            }
+        }
+
+        private void FinishBtn_Click(object sender, EventArgs e)
+        {
+            int score = 0;
+            CheckIfMatch(textBox1, labelValues[0], ref score, RnNum1);
+            CheckIfMatch(textBox2, labelValues[1], ref score, RnNum2);
+            CheckIfMatch(textBox3, labelValues[2], ref score, RnNum3);
+            CheckIfMatch(textBox4, labelValues[3], ref score, RnNum4);
+            CheckIfMatch(textBox5, labelValues[4], ref score, RnNum5);
+            ScoreList.Items.Add($"You guessed {score} numbers correctly");
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ValueGenerator();
+            ScoreList.Items.Clear();
+            LabelNumGenerator();
+            FlipVisible();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to Exit?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                    == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void LabelNumGenerator()
+        {
+            RnNum1.Text = labelValues[0].ToString();
+            RnNum2.Text = labelValues[1].ToString();
+            RnNum3.Text = labelValues[2].ToString();
+            RnNum4.Text = labelValues[3].ToString();
+            RnNum5.Text = labelValues[4].ToString();
+        }
+
+        private void FlipVisible()
+        {
+            RnNum1.Visible = !RnNum1.Visible;
+            RnNum2.Visible = !RnNum2.Visible;
+            RnNum3.Visible = !RnNum3.Visible;
+            RnNum4.Visible = !RnNum4.Visible;
+            RnNum5.Visible = !RnNum5.Visible;
+        }
+
+        private void ValueGenerator()
+        {
+            for (int counter = 0; counter < LabelAmount; counter++)
+            {
+                labelValues[counter] = randomGen.Next(101);
+            }
+        }
+
+         private void CheckIfMatch(TextBox textBox, int v, ref int score, Label rnNum)
+         {
+            if (Convert.ToInt32(textBox.Text) == v)
+            {
+                score++;
+                rnNum.ForeColor = SystemColors.ControlText;
+            }
+            else
+            {
+                rnNum.ForeColor = Color.Red;
+            }
+         }
+
+    }
+}
